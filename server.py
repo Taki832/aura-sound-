7698bbe86a31521c9f0549d8e841f98f24d04f40
Vishnu <vishnu@local>
2026-07-17 13:39:14 +0530

    Fix Pyrogram loop mismatch issue

--- server.py
+++ server.py
@@ -394,4 +394,6 @@ async def main():
     await asyncio.Event().wait()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
